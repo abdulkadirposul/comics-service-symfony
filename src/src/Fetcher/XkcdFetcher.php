93a82a45b0ca6d93634e27768aa963ce2ab1c9c5
abdulkadirposul <abdulkadir.posul@gmail.com>
2021-04-01 01:22:26 +0300

    XkcdFethcer is implemented

--- src/src/Fetcher/XkcdFetcher.php
+++ src/src/Fetcher/XkcdFetcher.php
@@ -3,24 +3,89 @@
 namespace App\Fetcher;
 
 use App\Fetcher\Abstracts\XkcdFetcherAbstract;
+use Exception;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class XkcdFetcher extends XkcdFetcherAbstract
 {
     private HttpClientInterface $client;
+    private string $domain;
 
     public function __construct(HttpClientInterface $client)
     {
         $this->client = $client;
+        $this->domain = 'https://xkcd.com';
     }
 
+    /**
+     * @return array|int[]
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
     public function fetch(): array
+    {
+        try {
+
+            $currentComics = $this->performRequest($this->domain . '/info.0.json');
+            if (!isset($currentComics["num"])) {
+                return [];
+            }
+
+            $currentNum = $currentComics["num"];
+            $returnArray = [];
+
+            //make api calls as much as demanded with length
+            $comicsCursorNum = $currentNum;
+            for ($i = 0; $i < $this->length && $comicsCursorNum > 0; $i++) {
+                $comicsCursorNum = $currentNum - $i;
+                $comicsCursorUrl = $this->domain . "/" . $comicsCursorNum . "/info.0.json";
+
+                $resultArray = $this->performRequest($comicsCursorUrl);
+                $returnArray[] = [
+                    'picture_url' => $resultArray['img'],
+                    'title' => $resultArray['title'],
+                    'description' => $resultArray['alt'],
+                    'web_url' => $comicsCursorUrl,
+                    'publishing_date' => convertToDatetime($resultArray['year'], $resultArray['month'], $resultArray['day'])
+                ];
+            }
+
+        } catch (Exception $e) {
+            return [];
+        }
+
+        return $returnArray;
+    }
+
+    /**
+     * @param $url
+     * @return array
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws Exception
+     */
+    private function performRequest($url): array
     {
         $response = $this->client->request(
             'GET',
-            'https://xkcd.com/info.0.json'
+            $url
         );
-        dd($response);
-        return [1,2,3];
+
+        if ($response->getStatusCode() != 200) {
+            throw new Exception("Request Error");
+        }
+
+        return $response->toArray();
     }
 }
---
+++ src/src/Helpers/helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @param int $year
+ * @param int $month
+ * @param int $day
+ * @return DateTime
+ */
+function convertToDatetime(int $year, int $month, int $day): string
+{
+    return date('Y-m-d H:i:s',strtotime("$year-$month-$day"));
+}
