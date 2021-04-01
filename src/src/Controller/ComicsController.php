ff8b4167ab3f1e5554096b92ae29ea57bd6f6455
abdulkadirposul <abdulkadir.posul@gmail.com>
2021-04-02 00:06:51 +0300

    swagger is integrated

--- src/src/Controller/ComicsController.php
+++ src/src/Controller/ComicsController.php
@@ -6,22 +6,61 @@ use App\Controller\Traits\MakeItValidated;
 use App\Fetcher\FetcherPreparer;
 use App\Service\Contracts\ComicsServiceContract;
 use App\Validator\ComicsIndexValidator;
+use OpenApi\Annotations\Parameter;
+use OpenApi\Annotations\Schema;
+use OpenApi\Annotations\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ComicsController extends AbstractController
 {
     use MakeItValidated;
 
     /**
-     * @Route("/comics", name="comics")
+     * Lists the comics from different sources
+     *
+     * @Route("/api/comics", name="comics", methods={"GET"})
      * @param Request $request
      * @param ComicsServiceContract $comicsService
      * @param FetcherPreparer $fetcherPreparer
      * @return Response
+     *
+     *
+     * @\OpenApi\Annotations\Response(
+     *     response="200",
+     *     description="Successful"
+     * )
+     *
+     * @\OpenApi\Annotations\Response(
+     *     response="422",
+     *     description="Validation Error"
+     * )
+     *
+     * @Parameter(
+     *     name="xkcd_length",
+     *     in="query",
+     *     description="Number of comics demanded from Xkcd",
+     *     @Schema(
+     *          type="integer"
+     *      )
+     * )
+     *
+     * @Parameter(
+     *     name="poorly_drawn_lines_length",
+     *     in="query",
+     *     description="Number of comics demanded from poorly_drawn_lines_length",
+     *     @Schema(
+     *          type="integer"
+     *      )
+     * )
+     *
+     * @Tag(
+     *     name="Comics"
+     * )
      */
     public function index(Request $request, ComicsServiceContract $comicsService, FetcherPreparer $fetcherPreparer)
     {
